16111e401eefccf0cb414b376bf817b5415a3b71
Jan D <79925808+dark-r00t@users.noreply.github.com>
2022-10-13 19:13:32 -0400

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -181,7 +181,7 @@ alias emacs="emacsclient -c -a 'emacs'"
 alias pacmanspace="pacman -Qi | egrep '^(Name|Installed)' | cut -f2 -d':' | paste - - | column -t | sort -nrk 2 | grep MiB | bat"
 alias systemstatus="systemctl status sshd | bat --color=always  && systemctl status tor | bat --color=always"
 
-alias yt="ytfzf -t --pages=5 --force-youtube --loop --async-thumbnails --ytdl-path='/usr/bin/yt-dlp'"
+alias yt="ytfzf -t --pages=5 --force-youtube --loop --async-thumbnails --ytdl-path=\'/usr/bin/yt-dlp\'"
 alias ytvideo="yt --type=video"
 alias ytmusic="ytvideo --audio-only"
 alias ytchannel="yt --type=channel"
